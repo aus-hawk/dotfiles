65c7395dcc4393c37aa3e84ceba3b6296a56753a
Austin Hawkins <aushawk0@gmail.com>
2022-10-07 22:48:48 -0500

    use `bat` as man page pager

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -10,5 +10,6 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 
 export MANWIDTH=80
+export MANPAGER="sh -c 'col -bx | bat -l man -p --theme=Nord'"
 export XINITRC="$XDG_CONFIG_HOME/X11/xinitrc"
 export GNUPGHOME="$XDG_DATA_HOME/gnupg"
