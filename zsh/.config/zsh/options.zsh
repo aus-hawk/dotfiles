676ce07b1392f86e98b0f81db045de34648a4d52
Austin Hawkins <aushawk0@gmail.com>
2022-10-11 23:51:31 -0500

    add git info to and improve general look of prompt

--- zsh/.config/zsh/options.zsh
+++ zsh/.config/zsh/options.zsh
@@ -1,3 +1,4 @@
 unsetopt beep
 setopt emacs
 setopt interactive_comments
+setopt prompt_subst
--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -1,4 +1,26 @@
-PROMPT="
-%F{1}%n%f@%F{4}%m%f %F{6}%B%~%b%f
-%F{2}%#%f "
-RPROMPT="%(?..[%?])"
+current-git-branch() {
+    if git rev-parse 2>/dev/null; then
+        info=" $(git branch --show-current)"
+        if [[ $(git status --porcelain) ]]; then
+            info="$info *"
+        fi
+        echo "$info"
+    fi
+}
+
+build-prompt() {
+    echo
+
+    p="[%F{4}%n%f@%F{4}%m%f]"
+    p="$p [%F{3}%~%f]"
+    g="$(current-git-branch)"
+    if [[ $g ]]; then
+        p="$p [%F{2}$g%f]"
+    fi
+    p="$p%(?.. [%F{1}%?%f])"
+    echo "$p"
+
+    echo ":: "
+}
+
+PROMPT='$(build-prompt)'
