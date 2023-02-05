3def3f2191e281b05b54d642fb581768f0fc8c55
Austin Hawkins <aushawk0@gmail.com>
2023-02-04 19:34:38 -0600

    Add mypy to null-ls

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -4,6 +4,19 @@ null_ls.setup {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.diagnostics.mypy.with {
+            extra_args = function(params)
+                local venv_locations = { "venv", ".venv" }
+                for _, venv in pairs(venv_locations) do
+                    venv = venv .. "bin/python"
+                    if vim.fn.empty(vim.fn.glob(venv)) ~= 0 then
+                        return { "--python-executable", venv }
+                    end
+                end
+                -- no virtual environment
+                return {}
+            end,
+        },
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
     },
